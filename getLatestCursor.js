c14e1b2942731a5d786087954be26dcd80694a27
Scott Lewis <scott@iconify.it>
2020-08-13 09:06:56 -0400

    Initial Commit

---
+++ getLatestCursor.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Require Logic
+const
+      lib    = require('lib')
+    , dotenv = require('dotenv')
+
+
+module.exports.handler = function(event, context) {
+    lib.getLatestCursor(event, function(error, response) {
+        return context.succeed(
+            lib.lambdaProxyResponse(200, response)
+        );
+    });
+};
---
+++ processFileList.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Require Logic
+const
+    lib    = require('lib')
+    , dotenv = require('dotenv')
+
+
+module.exports.handler = function(event, context) {
+
+    lib.getFileList(event, function(error, response) {
+        return context.succeed(
+            lib.lambdaProxyResponse(200, response)
+        );
+    });
+
+
+};
