8d038bc918a89f25e4234248c5c85adf85d8fe67
Scott Lewis <scott@iconify.it>
2020-08-14 00:52:06 -0400

    Add getLatestCursor logic

--- getLatestCursor.js
+++ getLatestCursor.js
@@ -1,15 +1,32 @@
 'use strict';
 
-// Require Logic
 const
       lib    = require('./lib')
-    , dotenv = require('dotenv')
+    , dotenv     = require('dotenv')
+    , uploadToS3 = require('./lib/functions/uploadToS3.js')
+    , storePDF   = require('./lib/functions/storePdf.js')
+    , formatDate = require('./lib/functions/formatDate.js')
+
+dotenv.config();
 
 
-module.exports.handler = function(event, context) {
-    lib.getLatestCursor(event, function(error, response) {
+module.exports.handler = (event, context) => {
+    lib.getLatestCursor(event, async (error, response) => {
+
+        const
+              bucket    = process.env.S3_BUCKET_NAME
+            , prefix    = 'cursor'
+            , storage   = uploadToS3({ bucket, prefix })
+            , uploadPDF = storePDF({ storage })
+
         return context.succeed(
-            lib.lambdaProxyResponse(200, response)
+            lib.lambdaProxyResponse(
+                200,
+                await uploadPDF(
+                    Buffer.from(JSON.stringify(response)),
+                    `${formatDate(false, true)}-cursor.json`
+                )
+            )
         );
     });
 };
