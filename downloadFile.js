da43ee05d4b4f51655cd1682e6a857481bf1c6aa
Scott Lewis <scott@iconify.it>
2020-08-18 00:58:36 -0400

    Finish PDF merge routines

---
+++ downloadFile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const
+    lib    = require('./lib')
+    , dotenv = require('dotenv')
+
+dotenv.config();
+
+
+module.exports.handler = function(event, context) {
+    lib.downloadFile(event, function(error, result) {
+        if (error) {
+            throw error;
+        }
+
+        return context.succeed(
+            lib.lambdaProxyResponse(200, result)
+        );
+
+    });
+};
